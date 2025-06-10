49d66a58b6f98514e81711e2b67a5fef86fb3668
sebaparra <sebastian.parra1@virginiogomez.cl>
2025-06-10 20:44:28 +0000

    aqui se hizo un algoritmo capaz de encontar un numero en la lista

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,12 +1,11 @@
-numeros = (11, 22, 33, 44, 55, 66, 77, 88, 99)
+numeros = (11, 22, 33, 44, 55, 66, 77, 88, 99,)
 
 numero_buscar = int(input("Ingrese el numero que deseas buscar: "))
 
 if numero_buscar in numeros:
-    posicion = numeros.index(numero_buscar)
+    posición = numeros.index(numero_buscar)
 
     print(f"el numero {numero_buscar} se encuentra en la posicion {posicion} de la lista.")
 
 else:
     print(f"el numero {numero_buscar} no se encuentra en la lista")
-
