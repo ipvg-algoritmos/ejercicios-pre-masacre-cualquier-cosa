7a7398adc4fec39040d43907a897e109aafd07de
sebaparra <sebastian.parra1@virginiogomez.cl>
2025-06-10 21:04:24 +0000

    se hizo un algoritmo que permite ingresar lista de numero y muestra cual es el numero mayor

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,13 +1,12 @@
 numeros = []
 while True:
-    n = int(input("ingrese un numero (negativo para terminar): "))
+    n = int(input("Ingrese un número (negativo para terminar): "))
     if n < 0:
         break
     numeros.append(n)
 
 if numeros:
-    print("el numero mayor es:", max(numeros))
-    print("el numero menor es:", min(numeros))
+    print("El número mayor es:", max(numeros))
+    print("El número menor es:", min(numeros))
 else:
-    print("no se ingresaron numeros validos.")
-    
+    print("No se ingresaron números válidos. ")
